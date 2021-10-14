c0c92b52397cce56e6a9f44da1cb0e62c72765e9
Cedric Wider <wider.cedric@gmail.com>
2021-10-14 22:10:24 +0200

    Only show user's tasks when getAllTasks

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,11 +1,14 @@
+import { User } from 'src/auth/user.entity';
 import { EntityRepository, Repository } from 'typeorm';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { Task } from './task.entity';
 
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
-  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+  async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const query = this.createQueryBuilder('task');
+    query.where({ user });
+
     const { status, search } = filterDto;
     if (status) {
       query.andWhere('task.status = :status', { status });
@@ -16,6 +19,7 @@ export class TasksRepository extends Repository<Task> {
         { search: `%${search}%` },
       );
     }
+
     return await query.getMany();
   }
 }
